b4c77f32edc8edf00d863db327c5cf07c8bf538f
DanthonyS <59703244+DanthonyS@users.noreply.github.com>
2021-02-11 14:15:39 -0500

    .

--- Marshmallow Masters/Assets/Daniel-CanoeMinigame/CanoeTest.cs
+++ Marshmallow Masters/Assets/Daniel-CanoeMinigame/CanoeTest.cs
@@ -9,7 +9,7 @@ public class CanoeTest : MonoBehaviour
     public float forceToAdd;
     Gyroscope gyro;
     float rotZ;
-    public Text RotZDisplay;
+    public Text rotRateDisplay, RotZDisplay;
 
     // Start is called before the first frame update
     void Start()
@@ -22,10 +22,10 @@ public class CanoeTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*
-        //rotZ += Input.gyro.rotationRateUnbiased.z;
-        rotZ += Input.gyro.rotationRateUnbiased.z;
+        rotZ += gyro.rotationRateUnbiased.z;
 
+        RotZDisplay.text = "Rot Z = " + rotZ.ToString("F2");
+        rotRateDisplay.text = "rRate = " + gyro.rotationRateUnbiased;
 
         if (Input.GetButtonDown("Jump"))
         {
@@ -33,59 +33,6 @@ public class CanoeTest : MonoBehaviour
             rb.AddTorque(transform.forward * forceToAdd, ForceMode.VelocityChange);
         }
 
-        RotZDisplay.text = "Rot Z = " + rotZ.ToString("F2");
-        */
-
-        Quaternion referenceRotation = Quaternion.identity;
-        Quaternion deviceRotation = new Quaternion(0.5f, 0.5f, -0.5f, 0.5f) * Input.gyro.attitude * new Quaternion(0, 0, 1, 0); //DeviceRotation.Get();
-        Quaternion eliminationOfXY = Quaternion.Inverse(Quaternion.FromToRotation(referenceRotation * Vector3.forward, deviceRotation * Vector3.forward));
-        Quaternion rotationZ = eliminationOfXY * deviceRotation;
-        rotZ = rotationZ.eulerAngles.z;
-
-        RotZDisplay.text = "Rot Z = " + rotZ.ToString("F2");
     }
 
-    /*
-    void Temp()
-    {
-        private static bool gyroInitialized = false;
-
-    public static bool HasGyroscope
-    {
-        get
-        {
-            return SystemInfo.supportsGyroscope;
-        }
-    }
-
-    public static Quaternion Get()
-    {
-        if (!gyroInitialized)
-        {
-            InitGyro();
-        }
-
-        return HasGyroscope
-            ? ReadGyroscopeRotation()
-            : Quaternion.identity;
-    }
-
-    private static void InitGyro()
-    {
-        if (HasGyroscope)
-        {
-            Input.gyro.enabled = true;                // enable the gyroscope
-            Input.gyro.updateInterval = 0.0167f;    // set the update interval to it's highest value (60 Hz)
-        }
-        gyroInitialized = true;
-    }
-
-    private static Quaternion ReadGyroscopeRotation()
-    {
-        return new Quaternion(0.5f, 0.5f, -0.5f, 0.5f) * Input.gyro.attitude * new Quaternion(0, 0, 1, 0);
-    }
-}
-
-    }
-*/
 }
