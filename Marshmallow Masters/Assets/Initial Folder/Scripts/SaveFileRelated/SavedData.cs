f25a1f3a7d145fd52fdb11417af153a8bafc0f77
JonahGM_S <gonzajon@sheridancollege.ca>
2021-02-20 20:16:32 -0500

    Begun Working on Save Files

---
+++ Marshmallow Masters/Assets/Initial Folder/Scripts/SaveFileRelated/SavedData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class SavedData
+{
+    public string playerName;
+
+    public int marshmallows;
+    public int roastedMarshmallows;
+
+
+
+}
