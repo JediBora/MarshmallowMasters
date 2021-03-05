27187df651c6bb7f38c7a072df73be22d598d6be
JonahGM_S <gonzajon@sheridancollege.ca>
2021-03-05 16:47:49 -0500

    Shake Detection Implemented

---
+++ Marshmallow Masters/Assets/Initial Folder/Scripts/FlashlightBBrokeyRelated/PhysicsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Used during testing
+//Used to move an object when phone is shook
+public class PhysicsController : MonoBehaviour
+{
+    public float shakeForceMultiplier;
+    public Rigidbody2D[] shakingRigidbodies;
+
+    public void ShakeRigidbodies(Vector3 deviceAcceleration)
+    {
+        foreach (var rigidbody in shakingRigidbodies)
+        {
+            rigidbody.AddForce(deviceAcceleration * shakeForceMultiplier, ForceMode2D.Impulse);
+
+
+
+
+
+        }
+
+
+    }
+}
---
+++ Marshmallow Masters/Assets/Initial Folder/Scripts/FlashlightBBrokeyRelated/ShakeDetector.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ShakeDetector : MonoBehaviour
+{
+    public float shakeDetectionThreshold;
+    public float minimumShakeInterval;
+
+    private float squareShakeDetectionThreshold;
+    private float timeSinceLastShake;
+
+    //Used in conjunction with Physics Controller
+    //private PhysicsController physController;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        squareShakeDetectionThreshold = Mathf.Pow(shakeDetectionThreshold, 2);
+
+        //Used in conjunction with Physics Controller
+        //physController = GetComponent<PhysicsController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.acceleration.sqrMagnitude >= squareShakeDetectionThreshold && Time.unscaledTime >= timeSinceLastShake + minimumShakeInterval)
+        {
+            //Used in conjunction with Physics Controller
+            //physController.ShakeRigidbodies(Input.acceleration);
+            timeSinceLastShake = Time.unscaledTime;
+
+            Debug.Log("I've been shooketh!");
+
+        }
+
+
+
+    }
+}
