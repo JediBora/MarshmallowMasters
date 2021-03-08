00f7193336d429731be630fd874edbdde8184fb8
King_Ram <trollax88@gmail.com>
2021-03-07 19:37:33 -0500

    Changed the spawning method
    Need to add more timing stuff and multiple spawning into it.

---
+++ Marshmallow Masters/Assets/AntSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntSpawner : MonoBehaviour
+{
+    public List<GameObject> spawnPoints;
+    public GameObject ant;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnAnt();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    }
+
+    void SpawnAnt()
+    {
+        GameObject spawnPoint = spawnPoints[(Random.Range(0, 3))];
+        
+        GameObject newAnt = Instantiate(ant, spawnPoint.transform.position, Quaternion.identity);
+
+        newAnt.transform.parent = gameObject.transform;
+    }
+}
