a9930e9f8e3a795c726e50947cd1b863a352c79c
King_Ram <trollax88@gmail.com>
2021-03-22 15:13:23 -0400

    Fixed and changed spawning methods
    added ant image and ramped up the difficulty.

--- Marshmallow Masters/Assets/Initial Folder/Scripts/Samwich/AntSpawner.cs
+++ Marshmallow Masters/Assets/Initial Folder/Scripts/Samwich/AntSpawner.cs
@@ -6,13 +6,18 @@ public class AntSpawner : MonoBehaviour
 {
     public List<GameObject> spawnPoints;
     public GameObject ant;
+    public float antSpeed = 5;
+    public float antSpeedMax = 25;
     public float timerEnd = 3;
+    //based on the speed of the ant it will increase spawning by lowering the timerEnd
+    public int antSpawnIncreaseTime;
+    public float spawnTimeIncrease= 0.1f;
     float time = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
@@ -24,6 +29,21 @@ public class AntSpawner : MonoBehaviour
         {
             SpawnAnt();
             time = 0;
+            if (antSpeed < antSpeedMax)
+            {
+                antSpeed++;
+            }
+            if (antSpawnIncreaseTime <= antSpeed & timerEnd > 0.5)
+            {
+                timerEnd = timerEnd - spawnTimeIncrease;
+                
+
+            }
+            if (timerEnd <= 0.5)
+            {
+                timerEnd = 0.5f;
+            }
+          
         }
     }
 
@@ -32,6 +52,7 @@ public class AntSpawner : MonoBehaviour
         GameObject spawnPoint = spawnPoints[(Random.Range(0, 4))];
         Debug.Log(spawnPoint);
         GameObject newAnt = Instantiate(ant, spawnPoint.transform.position, Quaternion.identity);
+        newAnt.GetComponent<Squish>().speed = antSpeed;
 
         newAnt.transform.parent = gameObject.transform;
     }
