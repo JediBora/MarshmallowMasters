a551c0dcfab146b886e2fea759e48ed130ee0e95
JonahGM_S <59736650+JonahGM@users.noreply.github.com>
2021-03-29 11:07:32 -0400

    Implemented Showing Marshmallows

---
+++ Marshmallow Masters/Assets/Initial Folder/Scripts/Inventory/MarshmallowCount.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MarshmallowCount : MonoBehaviour
+{
+    //Accessing saved data to display
+    public LoadAndSave loadAndSave;
+
+    public TextMeshProUGUI shmellowCount;
+    public TextMeshProUGUI roastedShmellowCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shmellowCount.text = "Marshmallows: " + loadAndSave.savedData.marshmallows;
+        roastedShmellowCount.text = " Roasted Marshmallows: " + loadAndSave.savedData.roastedMarshmallows;
+    }
+}
