795389091717225eb7c3b0d3474dcf5da60f6896
OverlordVGS <57149179+OverlordVGS@users.noreply.github.com>
2021-03-29 15:36:00 -0400

    Code Fix (Flashlight Percents)
    Fixed the bug where the flashlight game didn't work.

--- Marshmallow Masters/Assets/Initial Folder/Scripts/FlashlightBBrokeyRelated/FlashlightDifficultyController.cs
+++ Marshmallow Masters/Assets/Initial Folder/Scripts/FlashlightBBrokeyRelated/FlashlightDifficultyController.cs
@@ -13,12 +13,6 @@ public class FlashlightDifficultyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         if (difficultyOne)
         {
             DifficultyOneActive();
