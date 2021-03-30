00b44a34bedb90f4881a7631f311f167462401d2
King_Ram <trollax88@gmail.com>
2021-03-30 19:58:33 -0400

    Added Ant functionality and fixed bugs

--- Marshmallow Masters/Assets/Initial Folder/Scripts/Samwich/Squish.cs
+++ Marshmallow Masters/Assets/Initial Folder/Scripts/Samwich/Squish.cs
@@ -5,16 +5,40 @@ using UnityEngine;
 public class Squish : MonoBehaviour
 {
     public float speed = 1f;
-
+    private GameObject AntSpawn;
     // Update is called once per frame
-    void Update()
+
+    private void Start()
+    {
+        AntSpawn = GameObject.Find("Canvas");
+    }
+    void FixedUpdate()
     {
         transform.Translate(Vector3.down * Time.deltaTime * speed);
+        
+    }
+
+    private void Update()
+    {
+       
+        if (transform.position.y < -600f)
+        {
+            Debug.Log("Past -600");
+            Destroy(gameObject);
+        }
     }
 
     public void squish()
     {
         Destroy(gameObject);
 
+    }
+
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision");
+        AntSpawn.GetComponent<AntSpawner>().lives--;
+        Destroy(gameObject);
     }
 }
