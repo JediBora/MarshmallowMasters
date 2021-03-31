2ebf3cce6871daed5a37f3d5bb1f8fd6c5a6ef87
JonahGM_S <59736650+JonahGM@users.noreply.github.com>
2021-03-31 00:25:59 -0400

    Implemented More Sounds and Begun Title Screen

---
+++ Marshmallow Masters/Assets/Initial Folder/Scripts/CampOverworld/OverworldSoundManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverworldSoundManager : MonoBehaviour
+{
+
+    public AudioClip bGMClip;
+    public AudioSource bGMPlayer;
+    public AudioListener audioListener;
+
+    public LoadAndSave loadAndSave;
+
+    public bool clipFinished;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (loadAndSave.savedData.toggleOnSFX && !clipFinished)
+        {
+            bGMPlayer.clip = bGMClip;
+
+            bGMPlayer.Play();
+            clipFinished = true;
+        }
+    }
+}
