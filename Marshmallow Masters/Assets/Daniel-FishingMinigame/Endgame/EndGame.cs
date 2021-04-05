70e2ec63aecc03f48044866559be2f8764c40569
DanthonyS <59703244+DanthonyS@users.noreply.github.com>
2021-04-05 11:32:11 -0400

    Deleted "SampleScenes"

--- Marshmallow Masters/Assets/Daniel-FishingMinigame/Endgame/EndGame.cs
+++ Marshmallow Masters/Assets/Daniel-FishingMinigame/Endgame/EndGame.cs
@@ -11,7 +11,7 @@ public class EndGame : MonoBehaviour
     public Text marshmallowsGained_txt;
     public Text timeRemaining_txt;
     public Text timeRemainingShadow_txt;
-    public int timeRemaining = 60; //seconds
+    public int timeRemaining = 60; // minute by default
     
 
     // Start is called before the first frame update
@@ -31,7 +31,10 @@ public class EndGame : MonoBehaviour
             yield return new WaitForSeconds(1.0f);
         }
 
+        // When the timer ends, the game over screen appears
         GameOverScreen.SetActive(true);
+
+        // Marshmallows gained are counted (3 seconds)
         int marshmallowsGained = GetMarshmallowsGained();
 
         for (int i = 0; i < marshmallowsGained + 1; i++)
@@ -41,15 +44,21 @@ public class EndGame : MonoBehaviour
             yield return new WaitForSeconds(3 / (float) marshmallowsGained);
         }
 
+        // After marshmallows are counted, you can return to camp (button is greyed out while counting)
         BackToCampButton.interactable = true;
-        // >>>Save<<<
+        
+        // Save
     }
 
+
+    // Called when you want to quit marshmallow roasting. It can also be used for minigames that you're able to 'die' before timer runs out.
     public void SkipToEndOfCountdown()
     {
         timeRemaining = 0;
     }
 
+
+    // When the timer ends, retrieve the amount of marshmallows gained from the respective game manager (or whatever is tracking your score).
     int GetMarshmallowsGained()
     {
         string sceneName = SceneManager.GetActiveScene().name;
@@ -80,9 +89,12 @@ public class EndGame : MonoBehaviour
         }
     }
 
+
+    // Called when the "Back to Camp" button is pressed.
     public void ReturnToCamp()
     {
         //Debug.Log("Pressed");
         SceneManager.LoadScene("CampOverworld");
     }
+
 }
