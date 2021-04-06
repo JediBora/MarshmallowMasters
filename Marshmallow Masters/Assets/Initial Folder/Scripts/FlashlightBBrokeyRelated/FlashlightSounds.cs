b688b1d604fdd5c43c84e6c268851048aa3899f1
JonahGM_S <59736650+JonahGM@users.noreply.github.com>
2021-04-06 14:58:21 -0400

    Made SoundManager Modular and Added Sound to Marshmallow Roasting

--- Marshmallow Masters/Assets/Initial Folder/Scripts/FlashlightBBrokeyRelated/FlashlightSounds.cs
+++ Marshmallow Masters/Assets/Initial Folder/Scripts/FlashlightBBrokeyRelated/FlashlightSounds.cs
@@ -11,12 +11,12 @@ public class FlashlightSounds : MonoBehaviour
     public AudioClip[] bGM;
 
     //Sources to be played
-    public AudioSource audioSourceShake;
-    public AudioSource audioSourceClickOn;
-    public AudioSource audioSourceClickOff;
+    //This one loops
+    public AudioSource audioSourceSFX1;
+    //This one doesn't
+    public AudioSource audioSourceSFX2;
 
-
-
+    //This loops
     public AudioSource audioSourceBGMPlayer;
 
     //The single audio listener
@@ -32,51 +32,76 @@ public class FlashlightSounds : MonoBehaviour
     //used to check if bgm or songs are toggled
     public LoadAndSave loadAndSave;
 
+    //Used to identify which games are being played
+    public bool flashlightBBrokey;
+    public bool marshmallowRoasting;
 
-    public void Awake()
-    {
-        if (loadAndSave.savedData.toggleOnBGM)
-        {
-            PlayBGM();
+    public bool bgmIsPlaying;
 
-
-
-        }
-    }
 
 
     // Update is called once per frame
     void Update()
     {
         if (loadAndSave.savedData.toggleOnSFX)
-            {
-            if (shakeDetector.deviceIsShaking)
-            {
-                PlayRandomShake();
+        {
+            if (flashlightBBrokey)
+                FlashlightBBrokeySounds();
+            
 
-                if (flashlightClickPlayed)
-                {
-                    PlayClickOff();
-                    flashlightClickPlayed = false;
-
-                }
-            }
-            else if (!shakeDetector.deviceIsShaking)
-            {
-                if (!flashlightClickPlayed)
-                {
-                    PlayClickOn();
-                    flashlightClickPlayed = true;
-                }
-                shakeNoiseChosen = false;
-                audioSourceShake.Stop();
-            }
         }
 
-        
+        if (loadAndSave.savedData.toggleOnBGM && !bgmIsPlaying)
+        {
+            if (flashlightBBrokey)
+                PlayMarshmallowBGM();
+
+
+            if (marshmallowRoasting)
+                MarshmallowRoastingBGM();
+
+
+            bgmIsPlaying = true;
+        }
+
     }
 
-    public void PlayBGM()
+    public void MarshmallowRoastingBGM()
+    {
+        audioSourceBGMPlayer.clip = bGM[3];
+
+        audioSourceBGMPlayer.Play();
+    }
+
+
+    public void FlashlightBBrokeySounds()
+    {
+        if (shakeDetector.deviceIsShaking)
+        {
+            PlayRandomShake();
+
+            if (flashlightClickPlayed)
+            {
+                PlayClickOff();
+                flashlightClickPlayed = false;
+
+            }
+        }
+        else if (!shakeDetector.deviceIsShaking)
+        {
+            if (!flashlightClickPlayed)
+            {
+                PlayClickOn();
+                flashlightClickPlayed = true;
+            }
+            shakeNoiseChosen = false;
+            audioSourceSFX1.Stop();
+        }
+
+
+    }
+
+    public void PlayMarshmallowBGM()
     {
         if (difController.difficultyOne)
         {
@@ -101,27 +126,27 @@ public class FlashlightSounds : MonoBehaviour
     {
         if (!shakeNoiseChosen)
         {
-            audioSourceShake.clip = flashlightShake[Random.Range(0, flashlightShake.Length)];
+            audioSourceSFX1.clip = flashlightShake[Random.Range(0, flashlightShake.Length)];
 
 
-            audioSourceShake.Play();
+            audioSourceSFX1.Play();
 
             shakeNoiseChosen = true;
         }
-            
+
     }
 
     void PlayClickOn()
     {
-        audioSourceClickOn.clip = flashlightClick[0];
-        audioSourceClickOn.Play();
+        audioSourceSFX2.clip = flashlightClick[0];
+        audioSourceSFX2.Play();
 
     }
 
     void PlayClickOff()
     {
-        audioSourceClickOff.clip = flashlightClick[1];
-        audioSourceClickOff.Play();
+        audioSourceSFX2.clip = flashlightClick[1];
+        audioSourceSFX2.Play();
 
 
 
